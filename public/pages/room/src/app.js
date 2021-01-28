607f6d39d0becc6d9d277702eaf466a182ffde9f
Paolo Ramos <paolorr@gmail.com>
2021-01-28 19:39:16 -0300

    feat: connect to server via socketio

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -16,10 +16,12 @@ const onload = () => {
   // const recorderBtn = document.getElementById('record')
   // recorderBtn.addEventListener('click', recordClick(recorderBtn))
 
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder({ socketUrl })
   const view = new View()
   const media = new Media()
   const deps = {
-    view, media, room
+    view, media, room, socketBuilder
   }
 
   Business.initialize(deps)
